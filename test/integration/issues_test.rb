22c74bbc53df223a63f078ee52be7bedffb016c8
Yuya.Nishida <yuya@j96.org>
2022-10-12 13:17:01 +0900

    test: add

---
+++ test/integration/issues_test.rb
@@ -0,0 +1,104 @@
+require File.expand_path("../test_helper", __dir__)
+
+class RedmineRubyWasm::IssuesTest < RedmineRubyWasm::ApplicationSystemTestCase
+  RUBY_CODE_WITH_CODEBLOCK = <<~CODEBLOCK
+    ```ruby
+    %w[hello world!].map(&:capitalize).join(", ")
+    ```
+  CODEBLOCK
+  RUBY_CODE_RESULT = '"Hello, World!"'
+
+  let(:project) {
+    Project.create!(name: "foo", identifier: "foo", trackers: [tracker])
+  }
+  let(:role) {
+    Role.create!(name: "test role", permissions: %i[view_issues edit_issues])
+  }
+  let(:tracker) {
+    Tracker.new(name: "Defect").tap do |tracker|
+      tracker.default_status = IssueStatus.find_or_create_by!(name: "New")
+      tracker.save!
+    end
+  }
+  let(:user) {
+    User.create!(
+      login: "bob",
+      firstname: "Bob",
+      lastname: "Loblaw",
+      password: "password",
+      password_confirmation: "password",
+      mail: "bob.loblaw@example.com",
+    ).tap do |u|
+      Member.create!(
+        project: project,
+        principal: u,
+        roles: [role],
+      )
+    end
+  }
+  let(:issue_priority) { IssuePriority.find_or_create_by!(name: "Normal") }
+  let(:issue_status) { IssueStatus.find_or_create_by!(name: "New") }
+  let(:issue) {
+    Issue.create!(
+      project: project,
+      subject: "bar",
+      priority: issue_priority,
+      tracker: tracker,
+      author: user,
+      status: issue_status
+    )
+  }
+
+  setup do
+    Setting[:text_formatting] = "markdown"
+
+    login(user)
+  end
+
+  class CodeInIssue < self
+    CODE_SELECTOR = "#content > .issue > .description > .wiki > pre > code"
+
+    setup do
+      issue.reload.update!(description: RUBY_CODE_WITH_CODEBLOCK)
+    end
+
+    test "display result when code is clicked" do
+      visit(issue_path(issue))
+      assert_css("*[role=dialog]", visible: :hidden) # wait for creating node
+
+      first(CODE_SELECTOR).click
+      assert_css("*[role=dialog]")
+      assert_selector_inner_text_equal("#ruby-output-dialog", RUBY_CODE_RESULT)
+    end
+  end
+
+  class CodeInComment < self
+    CODE_SELECTOR = "#history .note > .wiki > pre > code"
+
+    setup do
+      issue.journals.create!(user: user, notes: RUBY_CODE_WITH_CODEBLOCK)
+    end
+
+    test "display result when code is clicked" do
+      visit(issue_path(issue))
+      assert_css("*[role=dialog]", visible: :hidden) # wait for creating node
+
+      first(CODE_SELECTOR).click
+      assert_css("*[role=dialog]")
+      assert_selector_inner_text_equal("#ruby-output-dialog", RUBY_CODE_RESULT)
+    end
+  end
+
+  private
+
+  def login(user)
+    visit(my_page_path)
+    assert_equal("/login", current_path)
+    within("#login-form form") do
+      fill_in("username", with: user.login)
+      fill_in("password", with: user.password)
+      find("input[name=login]").click
+    end
+    assert_equal(my_page_path,  current_path)
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,50 @@
 # Load the Redmine helper
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+require File.expand_path("../../../test/test_helper", __dir__)
+
+module RedmineRubyWasm::Lettable
+  def let(name, &block)
+    instance_variable_name = :"@__let__#{name}"
+    define_method(name) {
+      if instance_variable_defined?(instance_variable_name)
+        instance_variable_get(instance_variable_name)
+      else
+        instance_exec(&block).tap do |result|
+          instance_variable_set(instance_variable_name, result)
+        end
+      end
+    }
+  end
+
+  def let!(name, &block)
+    let(name, &block)
+    setup(name)
+  end
+end
+
+module RedmineRubyWasm::SystemTestAssertions
+  extend ActiveSupport::Concern
+
+  def assert_selector_inner_text_equal(selector, expected_text)
+    assert_equal(expected_text, first(selector).text)
+  end
+end
+
+class RedmineRubyWasm::ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  extend RedmineRubyWasm::Lettable
+  include RedmineRubyWasm::SystemTestAssertions
+
+  driven_by(
+    :selenium,
+    using: :headless_chrome,
+    screen_size: [1024, 900], # same as https://github.com/redmine/redmine/blob/5.0.2/test/application_system_test_case.rb#L49
+  ) do |driver_option|
+    ENV.fetch("GOOGLE_CHROME_ARGS", "").split(",").each do |chrome_argument|
+      driver_option.add_argument(chrome_argument)
+    end
+  end
+
+  setup do
+    Setting.delete_all
+    Setting.clear_cache
+  end
+end
